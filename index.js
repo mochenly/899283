ea501c974070e8167a06484458c1d42c88b79c8b
Monblant <dope.deala123@gmail.com>
2025-10-20 03:59:19 +0300

    fix: extra refresh on every swipe

--- index.js
+++ index.js
@@ -466,13 +466,11 @@ jQuery(async () => {
                     firstSwipeBlockExtra(messageId);
                 }
                 await handleUserTrigger(messageId - 1, true);
-            } else {
-                await swipeBlockExtra(messageId, current_swipe_id);
-            }
+            }     
         } else {
             await checkBlocksInFirstMessage();
-            await swipeBlockExtra(messageId, current_swipe_id);
         }
+        await swipeBlockExtra(messageId, current_swipe_id);
     });
 
     eventSource.on("/fatpresets/import/extblocks", ({ setObject, returnCode }) => {
